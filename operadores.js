5ab8aa352e224d4957fe002ba5807064affc6afe
AlessandroHetfield <160076296+AlessandroHetfield@users.noreply.github.com>
2024-05-05 09:56:02 -0400

    Add files via upload

---
+++ operadores.js
@@ -0,0 +1,12 @@
+/*var x=5;
+x++;
+console.log(`${x}`);
+x+=2;
+console.log(`${x}`);
+var y=3;
+console.log(`${x+=y}`);
+*/
+var x = 3;
+var y = x++;
+y += 1;
+console.log(`Tercer Proceso ${y += 1}`);
